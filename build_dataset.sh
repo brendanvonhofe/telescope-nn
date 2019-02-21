4197032bc3227f6f3e9cfa5d75b553d6ba3b1219
Brendan <bvh999@gmail.com>
2019-02-20 21:40:10 -0500

    updated dataset build script

---
+++ build_dataset.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir data/processed
+mkdir data/processed/train
+mkdir data/processed/train/fg
+mkdir data/processed/train/bg
+mkdir data/processed/train/mattes
+
+unzip data/raw/Adobe_Deep_Matting_Dataset.zip
+cp Combined_Dataset/Training_set/Adobe-licensed\ images/alpha/* data/processed/train/mattes
+cp Combined_Dataset/Training_set/Adobe-licensed\ images/fg/* data/processed/train/fg
+cp Combined_Dataset/Training_set/Other/fg/* data/processed/train/fg
+cp Combined_Dataset/Training_set/Other/alpha/* data/processed/train/mattes
+
+rm -r Combined_Dataset
+
+gsutil -m rsync gs://images.cocodataset.org/train2014 data/processed/train/bg
+
+file="src/utils/coco_removed_ims_2d.txt"
+while FS= read line
+do
+	rm "data/processed/train/bg/$line"
+done <"$file"
--- src/utils/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-file="coco_removed_ims_2d.txt"
-while FS= read line
-do
-	echo "$line"
-done <"$file"
