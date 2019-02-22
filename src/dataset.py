79d6c474f87a6dd19c57f65fe03b33d331be2e1c
Brendan <bvh999@gmail.com>
2019-02-21 19:51:47 -0500

    improved trimap generation

--- src/dataset.py
+++ src/dataset.py
@@ -6,32 +6,18 @@ from skimage import io, util, morphology
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
+import random
 
 class MatteDataset(Dataset):    
     def __init__(self, filenames, root_dir, fg_path, transform=None):
         self.fns = filenames
         self.root_dir = root_dir
         self.transform = transform
-        self.kernel = np.ones((32,32), np.uint8)
         self.fg_path = fg_path
         self.fgs = os.listdir(self.fg_path)
-        self.vmap1 = np.vectorize(self.mapto255)
-        self.vmap2 = np.vectorize(self.mapto255_2)
         
     def __len__(self):
         return len(self.fns)
-        
-    def mapto255(self, x):
-        if(x > 0):
-            return 255.
-        else:
-            return 0.
-        
-    def mapto255_2(self, x):
-        if(x > 191):
-            return 255.
-        else:
-            return 0.
     
     def __getitem__(self, idx):
         # Read in foreground, background and alpha mask for foreground
@@ -54,10 +40,7 @@ class MatteDataset(Dataset):
         # Create trimap from mask by randomly dilating
         if(len(mask.shape) == 2):
             mask = np.expand_dims(mask, -1)
-        kernel = np.ones((np.random.randint(20, 40), np.random.randint(20, 40)), np.uint8)
-        trimap = np.copy(mask)
-        trimap[np.where((cv2.dilate(trimap[:,:,0], kernel=kernel) - \
-                     cv2.erode(trimap[:,:,0],kernel=kernel))!=0)] = 128
+        trimap = gen_trimap(mask)
         
         # Pad images 
         if(image.shape[0] < 333 or image.shape[1] < 333):
@@ -67,27 +50,15 @@ class MatteDataset(Dataset):
             fg = cv2.resize(fg, (333, 333))
             bg = cv2.resize(bg, (333, 333))
 
-        # h, w = image.shape[0], image.shape[1]
-        # if(h < 330 or w < 330):
-        #     pad = [330-h, 330-w]
-        #     if(pad[0] < 0):
-        #         pad[0] = 0
-        #     if(pad[1] < 0):
-        #         pad[1] = 0
-        #     h, w = h + pad[0], w + pad[1]
-        #     pad = [(0, pad[0]), (0, pad[1]), (0, 0)]
-        #     image = util.pad(image, pad, mode='reflect')
-        #     trimap = util.pad(trimap, pad, mode='reflect')
-        #     mask = util.pad(mask, pad, mode='reflect')
-        #     fg = util.pad(fg, pad, mode='reflect')
-        #     bg = util.pad(bg, pad, mode='reflect')
-
+        # Concatenate image and trimap to make (h, w, 4) shape input
         im_map = np.concatenate((image, trimap), axis=2)
+
+        # Scale to between 0 and 1
         im_map = np.multiply(im_map, (1/255))
         mask = np.multiply(mask, (1/255))
-            
+        
+        # Package input, label and fg/bg to reconstruct image to calculate loss
         sample = {'im_map': im_map, 'mask': mask, 'bg': bg, 'fg': fg}
-
         if self.transform:
             sample = self.transform(sample)
     
@@ -118,17 +89,10 @@ class RandomCrop(object):
         top = np.random.randint(0, h - new_h)
         left = np.random.randint(0, w - new_w)
 
-        im_map = im_map[top: top + new_h,
-                        left: left + new_w]
-        
-        mask = mask[top: top + new_h,
-                    left: left + new_w]
-
-        bg = bg[top: top + new_h,
-                    left: left + new_w]
-        
-        fg = fg[top: top + new_h,
-                    left: left + new_w]
+        im_map = im_map[top: top + new_h, left: left + new_w]
+        mask = mask[top: top + new_h, left: left + new_w]
+        bg = bg[top: top + new_h, left: left + new_w]
+        fg = fg[top: top + new_h, left: left + new_w]
 
         return {'im_map': im_map, 'mask': mask, 'bg': bg, 'fg': fg}
 
@@ -161,3 +125,15 @@ def getTrainValSplit(path):
 
 def getTransforms():
     return transforms.Compose([RandomCrop(320), ToTensor()])
+
+def gen_trimap(alpha):
+    k_size = random.choice(range(1, 5))
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (k_size, k_size))
+    dilated = cv2.dilate(alpha, kernel, iterations=np.random.randint(1, 20))
+    
+    trimap = np.zeros(alpha.shape)
+    trimap.fill(128)
+    trimap[alpha >= 255] = 255
+    trimap[dilated <= 0] = 0
+
+    return trimap.astype(np.uint8)
