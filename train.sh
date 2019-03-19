29106e1d26aef2e48e031722c522fb021dcbaa3c
btocoolman <bvh999@gmail.com>
2019-03-19 13:21:37 -0400

    new training schedule

--- train.sh
+++ train.sh
@@ -1,45 +1,41 @@
 #!/bin/bash
 
-source /home/bread/anaconda3/bin/activate telescope
-
-# python src/train.py \
-#     --checkpoint=fresh \
-#     --save_dir=models/cyclic_0 \
-#     --stage=0 \
-#     --early_cutoff=100 \
-#     --epochs=-1 \
-#     --batch_size=16 \
-#     --threads=8 \
-#     --weighted_loss=0
-
-# python src/train.py \
-#     --checkpoint=cyclic_0 \
-#     --save_dir=models/cyclic_1 \
-#     --stage=1 \
-#     --early_cutoff=100 \
-#     --epochs=-1 \
-#     --batch_size=16 \
-#     --threads=8 \
-#     --weighted_loss=0 
-
-# python src/train.py \
-#     --checkpoint=cyclic_1 \
-#     --save_dir=models/cyclic_2 \
-#     --stage=2 \
-#     --early_cutoff=100 \
-#     --epochs=-1 \
-#     --batch_size=16 \
-#     --threads=8 \
-#     --weighted_loss=0 
-
 python src/train.py \
-    --checkpoint=cyclic_2 \
-    --save_dir=models/test \
-    --stage=2 \
-    --early_cutoff=-1 \
-    --epochs=1 \
+    --checkpoint=fresh \
+    --save_dir=models/nudataset_0 \
+    --stage=0 \
+    --early_cutoff=100 \
+    --epochs=-1 \
     --batch_size=16 \
     --threads=8 \
-    --weighted_loss=1 
+    --weighted_loss=0
 
-conda deactivate
+python src/train.py \
+    --checkpoint=nudataset_0 \
+    --save_dir=models/nudataset_1 \
+    --stage=1 \
+    --early_cutoff=25 \
+    --epochs=-1 \
+    --batch_size=16 \
+    --threads=8 \
+    --weighted_loss=0 
+
+python src/train.py \
+    --checkpoint=nudataset_1 \
+    --save_dir=models/nudataset_2 \
+    --stage=2 \
+    --early_cutoff=150 \
+    --epochs=-1 \
+    --batch_size=16 \
+    --threads=8 \
+    --weighted_loss=0 
+
+# python src/train.py \
+#     --checkpoint=cyclic_2 \
+#     --save_dir=models/test \
+#     --stage=2 \
+#     --early_cutoff=-1 \
+#     --epochs=1 \
+#     --batch_size=16 \
+#     --threads=8 \
+#     --weighted_loss=1 
