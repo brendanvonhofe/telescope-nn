843d87c3d5e740897a837e571d9e83489f4cac98
Brendan <bvh999@gmail.com>
2019-03-31 17:00:41 -0400

    train.sh contains full training program instead of dev runs

--- train.sh
+++ train.sh
@@ -1,51 +1,41 @@
 #!/bin/bash
 
-# python src/train.py \
-#     --checkpoint=fresh \
-#     --save_dir=models/nudataset_0 \
-#     --stage=0 \
-#     --early_cutoff=100 \
-#     --epochs=-1 \
-#     --batch_size=16 \
-#     --threads=8 \
-#     --weighted_loss=0
-
-# python src/train.py \
-#     --checkpoint=nudataset_0 \
-#     --save_dir=models/nudataset_1 \
-#     --stage=1 \
-#     --early_cutoff=25 \
-#     --epochs=-1 \
-#     --batch_size=16 \
-#     --threads=8 \
-#     --weighted_loss=0 
-
-# python src/train.py \
-#     --checkpoint=nudataset_1 \
-#     --save_dir=models/nudataset_2 \
-#     --stage=2 \
-#     --early_cutoff=125 \
-#     --epochs=-1 \
-#     --batch_size=16 \
-#     --threads=8 \
-#     --weighted_loss=0 
+python src/train.py \
+    --checkpoint=fresh \
+    --save_dir=models/telescope_0 \
+    --stage=0 \
+    --early_cutoff=100 \
+    --epochs=-1 \
+    --batch_size=16 \
+    --threads=8 \
+    --weighted_loss=0
 
 python src/train.py \
-    --checkpoint=nuds_weighted \
-    --save_dir=models/nuds_weighted2 \
+    --checkpoint=telescope_0 \
+    --save_dir=models/telescope_1 \
+    --stage=1 \
+    --early_cutoff=25 \
+    --epochs=-1 \
+    --batch_size=16 \
+    --threads=8 \
+    --weighted_loss=0 
+
+python src/train.py \
+    --checkpoint=telescope_1 \
+    --save_dir=models/telescope_2 \
+    --stage=2 \
+    --early_cutoff=125 \
+    --epochs=-1 \
+    --batch_size=16 \
+    --threads=8 \
+    --weighted_loss=0 
+
+python src/train.py \
+    --checkpoint=telescope_2 \
+    --save_dir=models/telescope_3 \
     --stage=2 \
     --early_cutoff=250 \
     --epochs=-1 \
     --batch_size=16 \
     --threads=8 \
     --weighted_loss=1
-
-# python src/train.py \
-#     --checkpoint=cyclic_2 \
-#     --save_dir=models/test \
-#     --stage=2 \
-#     --early_cutoff=-1 \
-#     --epochs=1 \
-#     --batch_size=16 \
-#     --threads=8 \
-#     --weighted_loss=1 
